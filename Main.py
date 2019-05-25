2292166cd8c48a2c2ef11c6a0f56cceb9903d7a8
Valentyn Batii <lavolpenoire@gmail.com>
2019-05-25 17:29:35 +0300

    Add files via upload
    This is first update of my project with change of widget position structure

--- Main.py
+++ Main.py
@@ -4,14 +4,16 @@ from tkinter.ttk import *
 import os
 import vlc
 
+window = Tk()
+
 
 class PyMediaPlayer(Frame):
 
-    appbackground = '#d2d2d2'
-    widgetbackground = '#a19c9c'
-    activewidgetbackground = '#d4d8d4'
-    logocolor = '#326ada'
-    activelogocolor = '#433e90'
+    appbackground = '#e6ecf0'
+    widgetbackground = '#ffffff'
+    activewidgetbackground = '#737678'
+    logocolor = '#0184b4'
+    activelogocolor = '#0084b4'
 
     def __init__(self, parent):
         Frame.__init__(self, parent)
@@ -22,55 +24,61 @@ class PyMediaPlayer(Frame):
     def create_ui(self):
 
         self.parent.title("PyMediaPlayer")
-        self.pack(fill=BOTH, expand=1)
+        self.place(relwidth=1, relheight=1)
 
         style = Style()
         style.configure("TFrame", bg=self.appbackground)
-        style.configure("TButton", bg=self.widgetbackground)
+        style.configure("TButton", bg=self.widgetbackground, bd=0)
 
-        album_logo = Label(self, text="Logo of your Album", anchor=CENTER, background=self.widgetbackground)
-        album_logo.place(relx=0.125, rely=0, relwidth=0.75, relheight=0.5)
+        original_frame = Frame(window, style='TFrame')
+        original_frame.place(relx=0, rely=0, width=400, relheight=1)
 
-        song_title = Label(self, text="Artist - Song Name", anchor=CENTER, background=self.widgetbackground)
+        album_logo = Label(original_frame, text="Logo of your Album", anchor=CENTER, background=self.widgetbackground)
+        album_logo.place(relx=0.5, rely=0.25, width=300, height=250, anchor=CENTER)
+
+        song_title = Label(original_frame, text="Artist - Song Name", anchor=CENTER, background=self.widgetbackground)
         song_title.place(relx=0.5, rely=0.5, width=400, relheight=0.1, anchor=CENTER)
 
-        scroll_track_time = Scale(self, orient=HORIZONTAL)
+        scroll_track_time = Scale(original_frame, orient=HORIZONTAL)
         scroll_track_time.place(relx=0.5, rely=0.6, width=400, relheight=0.1, anchor=CENTER)
 
-        equalizer = Label(self, text="There will be equalizer", anchor=CENTER, background=self.widgetbackground)
-        equalizer.place(relx=0.5, rely=0.7, width=400, relheight=0.2, anchor=CENTER)
+        equalizer = Label(original_frame, text="There will be equalizer", anchor=CENTER, background=self.widgetbackground)
+        equalizer.place(relx=0.5, rely=0.7, width=400, relheight=0.1, anchor=CENTER)
 
-        play_button = Button(self, text="PLAY")
+        buttons_frame = Frame(original_frame, style='TFrame')
+        buttons_frame.place(relx=0.5, rely=0.8, width=400, height=50, anchor=CENTER)
+
+        play_button = Button(buttons_frame, text="PLAY")
         play_button.config(command=self.playsong)
-        play_button.place(relx=0, rely=0.9, relwidth=0.08, relheight=0.08)
+        play_button.place(relx=0, rely=0, relwidth=0.08, relheight=1)
 
-        pause_button = Button(self, text="PAUSE")
+        pause_button = Button(buttons_frame, text="PAUSE")
         pause_button.config(command=self.pausesong)
-        pause_button.place(relx=0.125, rely=0.9, relwidth=0.08, relheight=0.08)
+        pause_button.place(relx=0.125, rely=0, relwidth=0.08, relheight=1)
 
-        stop_button = Button(self, text="STOP")
+        stop_button = Button(buttons_frame, text="STOP")
         stop_button.config(command=self.stop)
-        stop_button.place(relx=0.250, rely=0.9, relwidth=0.08, relheight=0.08)
+        stop_button.place(relx=0.250, rely=0, relwidth=0.08, relheight=1)
 
-        echo_button = Button(self, text="ECHO")
+        echo_button = Button(buttons_frame, text="ECHO")
         echo_button.config(command=self.echo_effect)
-        echo_button.place(relx=0.375, rely=0.9, relwidth=0.08, relheight=0.08)
+        echo_button.place(relx=0.375, rely=0, relwidth=0.08, relheight=1)
 
-        volume_button = Button(self, text="VOLUME")
-        volume_button.config()
-        volume_button.place(relx=0.500, rely=0.9, relwidth=0.08, relheight=0.08)
+        volume_button = Button(buttons_frame, text="VOLUME")
+        volume_button.bind("<Enter>",self.volume)
+        volume_button.place(relx=0.500, rely=0, relwidth=0.08, relheight=1)
 
-        mute_button = Button(self, text="Mute")
+        mute_button = Button(buttons_frame, text="Mute")
         mute_button.config(command=self.mute)
-        mute_button.place(relx=0.625, rely=0.9, relwidth=0.08, relheight=0.08)
+        mute_button.place(relx=0.625, rely=0, relwidth=0.08, relheight=1)
 
-        open_file_button = Button(self, text="Find", style="TButton")
+        open_file_button = Button(buttons_frame, text="Find", style="TButton")
         open_file_button.config(command=self.extract_files)
-        open_file_button.place(relx=0.750, rely=0.9, relwidth=0.08, relheight=0.08)
+        open_file_button.place(relx=0.750, rely=0, relwidth=0.08, relheight=1)
 
-        configuration_button = Button(self, text="Change")
-        configuration_button.config()
-        configuration_button.place(relx=0.875, rely=0.9, relwidth=0.1, relheight=0.08)
+        configuration_button = Button(buttons_frame, text="Change")
+        configuration_button.config(command=self.sound_configuration)
+        configuration_button.place(relx=0.875, rely=0, relwidth=0.8, relheight=1)
 
     def playsong(self):
         self.player = vlc.MediaPlayer(self.filename)
@@ -83,15 +91,37 @@ class PyMediaPlayer(Frame):
         self.player.stop()
 
     def echo_effect(self):
-        vlc.libvlc_audio_set_delay(self.player, 1000)
-        vlc.libvlc_audio_get_delay(self.player)
+        filename = self.filename
+
+
+    @staticmethod
+    def volume(event=None):
+        volume_window = Toplevel(window)
+        wid = event.widget
+        try:
+            if volume_window.winfo_exists(): return
+        except:
+            pass
+        volume_var = IntVar(value=50)
+        volume_window.wm_overrideredirect(False)
+        volume_window.resizable(0,0)
+        volume_window.lift()
+        volume_window.attributes('-alpha', 0.6)
+        volume_window.attributes('-topmost', 1)
+        scale = Scale(volume_window, from_=0, to=100, variable=volume_var, orient='horizontal', bg='black', fg='white')
+        scale.pack()
+        width = int((scale.winfo_reqwidth() - wid.winfo_width()) / 2)
+        volume_window.geometry('+%s+%s' % (wid.winfo_windowx() - width, wid.winfo_windowy() - scale.winfo_reqheight()))
+        volume_window.focus()
+        volume_window.bind("<Leave>", volume_window.destroy())
 
     def mute(self):
         self.player.audio_set_mute(True)
 
     def sound_configuration(self):
-        yield
-        #vlc.AudioEqualizer
+        equalizer_window = Toplevel(window)
+        equalizer = vlc.AudioEqualizer()
+
 
     def extract_files(self):
         self.filename = askopenfilename(filetypes=[("Mp3 Files", "*.mp3;*.wav;")])
@@ -101,7 +131,6 @@ class PyMediaPlayer(Frame):
 
 
 def main():
-    window = Tk()
 
     width_monitor = window.winfo_screenwidth()
     height_monitor = window.winfo_screenheight()
