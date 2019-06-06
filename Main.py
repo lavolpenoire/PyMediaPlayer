6ee4a41cfd664d764d01c2d66186c6b84ed5a1fc
Valentyn Batii <lavolpenoire@gmail.com>
2019-06-06 05:11:24 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,8 +1,10 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter.ttk import *
+from PIL import ImageTk
 import os
 import vlc
+
 
 window = Tk()
 
@@ -20,6 +22,8 @@ class PyMediaPlayer(Frame):
         self.parent = parent
 
         self.create_ui()
+        self.extract_files()
+        self.create_player()
 
     def create_ui(self):
 
@@ -36,8 +40,9 @@ class PyMediaPlayer(Frame):
         album_logo = Label(original_frame, text="Logo of your Album", anchor=CENTER, background=self.widgetbackground)
         album_logo.place(relx=0.5, rely=0.25, width=300, height=250, anchor=CENTER)
 
-        song_title = Label(original_frame, text="Artist - Song Name", anchor=CENTER, background=self.widgetbackground)
-        song_title.place(relx=0.5, rely=0.5, width=400, relheight=0.1, anchor=CENTER)
+        song_tittle = "{} - {}".format(vlc.Meta.Artist, vlc.Meta.Title)
+        title_label = Label(original_frame, text=song_tittle, anchor=CENTER, background=self.widgetbackground)
+        title_label.place(relx=0.5, rely=0.5, width=400, relheight=0.1, anchor=CENTER)
 
         scroll_track_time = Scale(original_frame, orient=HORIZONTAL)
         scroll_track_time.place(relx=0.5, rely=0.6, width=400, relheight=0.1, anchor=CENTER)
@@ -46,42 +51,58 @@ class PyMediaPlayer(Frame):
         equalizer.place(relx=0.5, rely=0.7, width=400, relheight=0.1, anchor=CENTER)
 
         buttons_frame = Frame(original_frame, style='TFrame')
-        buttons_frame.place(relx=0.5, rely=0.8, width=400, height=50, anchor=CENTER)
+        buttons_frame.place(relx=0.5, rely=0.85, width=400, height=50, anchor=CENTER)
 
-        play_button = Button(buttons_frame, text="PLAY")
+        self.play_image = ImageTk.PhotoImage(file='play.png')
+        play_button = Button(buttons_frame, image=self.play_image)
         play_button.config(command=self.playsong)
-        play_button.place(relx=0, rely=0, relwidth=0.08, relheight=1)
+        play_button.place(relx=0, rely=0, width=50, height=50)
 
-        pause_button = Button(buttons_frame, text="PAUSE")
+        self.pause_image = ImageTk.PhotoImage(file='pause.png')
+        pause_button = Button(buttons_frame, image=self.pause_image)
         pause_button.config(command=self.pausesong)
-        pause_button.place(relx=0.125, rely=0, relwidth=0.08, relheight=1)
+        pause_button.place(relx=0.125, rely=0, width=50, height=50)
 
-        stop_button = Button(buttons_frame, text="STOP")
+        self.stop_image = ImageTk.PhotoImage(file='stop.png')
+        stop_button = Button(buttons_frame, image=self.stop_image)
         stop_button.config(command=self.stop)
-        stop_button.place(relx=0.250, rely=0, relwidth=0.08, relheight=1)
+        stop_button.place(relx=0.250, rely=0, width=50, height=50)
 
-        echo_button = Button(buttons_frame, text="ECHO")
+        self.echo_image = ImageTk.PhotoImage(file='reverb.png')
+        echo_button = Button(buttons_frame, image=self.echo_image)
         echo_button.config(command=self.echo_effect)
-        echo_button.place(relx=0.375, rely=0, relwidth=0.08, relheight=1)
+        echo_button.place(relx=0.375, rely=0, width=50, height=50)
 
-        volume_button = Button(buttons_frame, text="VOLUME")
-        volume_button.bind("<Enter>",self.volume)
-        volume_button.place(relx=0.500, rely=0, relwidth=0.08, relheight=1)
+        self.volume_image = ImageTk.PhotoImage(file='volume.png')
+        volume_button = Button(buttons_frame, image=self.volume_image)
+        volume_button.bind("<Enter>", self.volume)
+        volume_button.place(relx=0.500, rely=0, width=50, height=50)
 
-        mute_button = Button(buttons_frame, text="Mute")
+        self.mute_image = ImageTk.PhotoImage(file='mute.png')
+        mute_button = Button(buttons_frame, image=self.mute_image)
         mute_button.config(command=self.mute)
-        mute_button.place(relx=0.625, rely=0, relwidth=0.08, relheight=1)
+        mute_button.place(relx=0.625, rely=0, width=50, height=50)
 
-        open_file_button = Button(buttons_frame, text="Find", style="TButton")
+        self.search_image = ImageTk.PhotoImage(file='search.png')
+        open_file_button = Button(buttons_frame, image=self.search_image, style="TButton")
         open_file_button.config(command=self.extract_files)
-        open_file_button.place(relx=0.750, rely=0, relwidth=0.08, relheight=1)
+        open_file_button.place(relx=0.750, rely=0, width=50, height=50)
 
-        configuration_button = Button(buttons_frame, text="Change")
+        self.equalizer_image = ImageTk.PhotoImage(file='equalizer.png')
+        configuration_button = Button(buttons_frame, image=self.equalizer_image)
         configuration_button.config(command=self.sound_configuration)
-        configuration_button.place(relx=0.875, rely=0, relwidth=0.8, relheight=1)
+        configuration_button.place(relx=0.875, rely=0, width=50, height=50)
+
+    def extract_files(self):
+        self.filename = askopenfilename(filetypes=[("Mp3 Files", "*.mp3;*.wav;")])
+        self.filename = os.path.abspath(self.filename)
+        print(self.filename)
+        return self.filename
+
+    def create_player(self):
+        self.player = vlc.MediaPlayer(self.filename)
 
     def playsong(self):
-        self.player = vlc.MediaPlayer(self.filename)
         self.player.play()
 
     def pausesong(self):
@@ -91,43 +112,19 @@ class PyMediaPlayer(Frame):
         self.player.stop()
 
     def echo_effect(self):
-        filename = self.filename
+        yield
 
-
-    @staticmethod
-    def volume(event=None):
-        volume_window = Toplevel(window)
-        wid = event.widget
-        try:
-            if volume_window.winfo_exists(): return
-        except:
-            pass
-        volume_var = IntVar(value=50)
-        volume_window.wm_overrideredirect(False)
-        volume_window.resizable(0,0)
-        volume_window.lift()
-        volume_window.attributes('-alpha', 0.6)
-        volume_window.attributes('-topmost', 1)
-        scale = Scale(volume_window, from_=0, to=100, variable=volume_var, orient='horizontal', bg='black', fg='white')
-        scale.pack()
-        width = int((scale.winfo_reqwidth() - wid.winfo_width()) / 2)
-        volume_window.geometry('+%s+%s' % (wid.winfo_windowx() - width, wid.winfo_windowy() - scale.winfo_reqheight()))
-        volume_window.focus()
-        volume_window.bind("<Leave>", volume_window.destroy())
+    def volume(self):
+        yield
 
     def mute(self):
         self.player.audio_set_mute(True)
 
     def sound_configuration(self):
         equalizer_window = Toplevel(window)
-        equalizer = vlc.AudioEqualizer()
+        equalizer_window.geometry('300x200')
 
 
-    def extract_files(self):
-        self.filename = askopenfilename(filetypes=[("Mp3 Files", "*.mp3;*.wav;")])
-        self.filename = os.path.abspath(self.filename)
-        print(self.filename)
-        return self.filename
 
 
 def main():
